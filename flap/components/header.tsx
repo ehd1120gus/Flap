cf4b684db85096c5f95e73be5683dc223693d8d7
ehd1120gus <62318147+ehd1120gus@users.noreply.github.com>
2024-01-24 23:43:34 +0900

    [Modi] Header prototyping

--- flap/components/header.tsx
+++ flap/components/header.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 import FlapLogo from "./flaplogo";
 import { HiOutlineMenu, HiOutlineSearch } from "react-icons/hi";
+import { IoAdd } from "react-icons/io5";
+import { VscInbox } from "react-icons/vsc";
+import {
+  GoCommandPalette,
+  GoTriangleDown,
+  GoIssueOpened,
+  GoGitPullRequest,
+} from "react-icons/go";
 
 export default function Header() {
   return (
@@ -18,12 +26,34 @@ export default function Header() {
           Product
         </div>
       </div>
-      <div className="flex justify-end w-[500px]">
+      <div className="flex ml-auto space-x-2 items-center">
         <div className="flex h-8 w-60 bg-transparent rounded-md border border-neutral-700 hover:border-neutral-400 cursor-pointer items-center">
-          <HiOutlineSearch className="h-4 w-4 text-neutral-400" />
-          <div className="text-white text-opacity-70 text-sm font-['SF Pro Text'] leading-tight">
-            Search GitHub
+          <HiOutlineSearch className="h-4 w-4 mx-2 text-neutral-400" />
+          <div className="text-neutral-400 text-sm font-['SF Pro Text'] ">
+            Type / to search
           </div>
+          <div className="flex ml-auto items-center">
+            <div className="text-neutral-400 text-sm font-thin font-['SF Pro Text'] ">
+              |
+            </div>
+            <GoCommandPalette className="h-4 w-4 mx-2 text-neutral-400 justify-center" />
+          </div>
+        </div>
+        <div className="text-neutral-400 text-md font-thin font-['SF Pro Text'] ">
+          |
+        </div>
+        <div className="flex h-8 bg-transparent rounded-md border border-neutral-700 hover:border-neutral-400 cursor-pointer items-center justify-center px-2">
+          <IoAdd className="h-5 w-5 text-neutral-400" />
+          <GoTriangleDown className="h-4 w-4 text-neutral-400" />
+        </div>
+        <div className="flex h-8 w-8 bg-transparent rounded-md flex-col border border-neutral-700 hover:border-neutral-400 cursor-pointer items-center justify-center">
+          <GoIssueOpened className="h-4 w-4 text-neutral-400" />
+        </div>
+        <div className="flex h-8 w-8 bg-transparent rounded-md flex-col border border-neutral-700 hover:border-neutral-400 cursor-pointer items-center justify-center">
+          <GoGitPullRequest className="h-4 w-4 text-neutral-400" />
+        </div>
+        <div className="flex h-8 w-8 bg-transparent rounded-md flex-col border border-neutral-700 hover:border-neutral-400 cursor-pointer items-center justify-center">
+          <VscInbox className="h-4 w-4 text-neutral-400" />
         </div>
         <div className="px-3 py-1.5 justify-start items-center gap-2 inline-flex">
           <div className="text-white font-semibold font-['SF Pro Text'] leading-tight">
